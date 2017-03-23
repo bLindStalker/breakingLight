b27889211307142aa0c5f7200469dac5b73a5732
AYakimenko <AYakimenko@playtika.coum>
2017-03-23 18:18:43 +0200

    logic update

--- core/src/com/breaking/game/Constants.java
+++ core/src/com/breaking/game/Constants.java
@@ -29,9 +29,9 @@ public final class Constants {
 
     public static final int MAX_ACTIVE_LAMPS = 9;
 
-    public static final float MIN_ACTIVE_LAMP_TIME = 0.6f;
-    public static final float MAX_ACTIVE_LAMP_TIME = 1.1f;
-    public static final float ACTIVE_LAMP_INTERVAL = 1.3f;
+    public static final float MIN_ACTIVE_LAMP_TIME = 0.7f;
+    public static final float MAX_ACTIVE_LAMP_TIME = 1.2f;
+    public static final float ACTIVE_LAMP_INTERVAL = 1.4f;
 
     public static final float MIN_BROKEN_TIME = 0.8f;
     public static final float MAX_BROKEN_TIME = 1.1f;
@@ -41,5 +41,5 @@ public final class Constants {
     public static final float MAX_TURN_OFF_TIME = 0.7f;
     public static final float TURN_OFF_TIME_INTERVAL = 0.7f;
 
-    public static final int HARD_CORE_TIME = 13;
+    public static final int HARD_CORE_TIME = 15;
 }
