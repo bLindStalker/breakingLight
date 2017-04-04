525b349d55a085a8c39f777519e0cee446c3040c
AYakimenko <Ayakimenko@playtika.com>
2017-04-04 15:58:58 +0300

    optimize code

--- core/src/com/breaking/game/screens/menu/MainMenu.java
+++ core/src/com/breaking/game/screens/menu/MainMenu.java
@@ -9,15 +9,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Timer;
-import com.breaking.game.AssetLoader;
-import com.breaking.game.Constants;
 import com.breaking.game.Main;
 import com.breaking.game.actors.ImageActor;
 import com.breaking.game.screens.GameScreen;
 
 import static com.badlogic.gdx.graphics.Color.FIREBRICK;
 import static com.breaking.game.AssetLoader.BUTTON_TEXT_COLOR;
+import static com.breaking.game.AssetLoader.getButtonStyle;
 import static com.breaking.game.AssetLoader.getFont;
+import static com.breaking.game.AssetLoader.getLabel;
 import static com.breaking.game.Constants.HEIGHT;
 import static com.breaking.game.Constants.WIDTH;
 import static com.breaking.game.Constants.X_CENTER;
@@ -30,22 +30,41 @@ public class MainMenu extends Group {
     private static final int MENU_BUTTON_WIDTH = 400;
     private static final int MENU_BUTTON_HEIGHT = 140;
     private static final int MENU_BUTTON_WHITE_SPACE = 40;
-    private static final int X_MENU_BUTTON_POSITION = Constants.X_CENTER - 200;
+    private static final int X_MENU_BUTTON_POSITION = X_CENTER - 200;
     private static final int Y_MENU_BUTTON = 500;
     private static final int LOGO_WIDTH = 700;
 
+    private final Main main;
+    private final Group menuGroup;
+
     public MainMenu(final Main main, final Group menuGroup) {
-        addActor(new ImageActor((WIDTH - LOGO_WIDTH) / 2, HEIGHT - 385 - 130, LOGO_WIDTH, 385, AssetLoader.getLabel()));
+        addActor(new ImageActor((WIDTH - LOGO_WIDTH) / 2, HEIGHT - 385 - 130, LOGO_WIDTH, 385, getLabel()));
+        this.main = main;
+        this.menuGroup = menuGroup;
+
+        addActor(buildScoreLabel());
+        addActor(buildButtons());
+    }
+
+    private Group buildScoreLabel() {
+        Group labelGroup = new Group();
 
         int score = getScore();
-        addActor(buildScoreLabel(getFont(BUTTON_TEXT_COLOR), "Max: ", 780, X_CENTER - 300, score > 0));
-        addActor(buildScoreLabel(getFont(FIREBRICK), valueOf(score), 780, X_CENTER - 190, score > 0));
+        labelGroup.addActor(buildScoreLabel(getFont(BUTTON_TEXT_COLOR), "Max: ", 780, X_CENTER - 300));
+        labelGroup.addActor(buildScoreLabel(getFont(FIREBRICK), valueOf(score), 780, X_CENTER - 190));
 
         int totalScore = getTotalScore();
-        addActor(buildScoreLabel(getFont(BUTTON_TEXT_COLOR), "Total: ", 720, X_CENTER - 300, totalScore > 0));
-        addActor(buildScoreLabel(getFont(FIREBRICK), valueOf(totalScore), 720, X_CENTER - 175, totalScore > 0));
+        labelGroup.addActor(buildScoreLabel(getFont(BUTTON_TEXT_COLOR), "Total: ", 720, X_CENTER - 300));
+        labelGroup.addActor(buildScoreLabel(getFont(FIREBRICK), valueOf(totalScore), 720, X_CENTER - 175));
 
-        TextButton startButton = new TextButton("START", AssetLoader.getButtonStyle());
+        labelGroup.setVisible(score > 0 && totalScore > 0);
+        return labelGroup;
+    }
+
+    private Group buildButtons() {
+        Group menuButtonsGroup = new Group();
+
+        TextButton startButton = new TextButton("START", getButtonStyle());
         startButton.setBounds(X_MENU_BUTTON_POSITION, Y_MENU_BUTTON, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
         startButton.addListener(new ClickListener() {
             @Override
@@ -59,9 +78,9 @@ public class MainMenu extends Group {
                 }, 0.25f);
             }
         });
-        addActor(startButton);
+        menuButtonsGroup.addActor(startButton);
 
-        TextButton advancedButton = new TextButton("ADVANCED", AssetLoader.getButtonStyle());
+        TextButton advancedButton = new TextButton("ADVANCED", getButtonStyle());
         advancedButton.setBounds(X_MENU_BUTTON_POSITION, Y_MENU_BUTTON - MENU_BUTTON_HEIGHT - MENU_BUTTON_WHITE_SPACE, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
         advancedButton.addListener(new ClickListener() {
             @Override
@@ -69,9 +88,9 @@ public class MainMenu extends Group {
                 menuGroup.addAction(Actions.moveTo(-WIDTH, 0, MENU_SWITCH_TIME));
             }
         });
-        addActor(advancedButton);
+        menuButtonsGroup.addActor(advancedButton);
 
-        TextButton exitButton = new TextButton("EXIT", AssetLoader.getButtonStyle());
+        TextButton exitButton = new TextButton("EXIT", getButtonStyle());
         exitButton.setBounds(X_MENU_BUTTON_POSITION, Y_MENU_BUTTON - (2 * MENU_BUTTON_HEIGHT) - (2 * MENU_BUTTON_WHITE_SPACE), MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
         exitButton.addListener(new ClickListener() {
             @Override
@@ -79,15 +98,16 @@ public class MainMenu extends Group {
                 Gdx.app.exit();
             }
         });
-        addActor(exitButton);
+        menuButtonsGroup.addActor(exitButton);
+        return menuButtonsGroup;
     }
 
-    private Label buildScoreLabel(Label.LabelStyle font, String text, int y, int x, boolean visible) {
+    private Label buildScoreLabel(Label.LabelStyle font, String text, int y, int x) {
         Label label = new Label(text, font);
+
         label.setAlignment(Align.left);
         label.setBounds(x, y, 100, 50);
         label.setFontScale(0.7f, 0.6f);
-        label.setVisible(visible);
 
         return label;
     }
